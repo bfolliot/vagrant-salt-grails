c8def72a0b57d7dc307fd217e3929a470e1c988a
Bryan Folliot <contact@bryanfolliot.fr>
2013-10-06 15:25:05 +0200

    Update env var

--- Shares/etc/profile.d/grails.sh
+++ Shares/etc/profile.d/grails.sh
@@ -1,3 +1,3 @@
-GRAILS_HOME=/srv/grails
-GRAILS_OPTS="-server -noverify -Xmx1G -Xms1G -XX:MaxPermSize=512m -XX:PermSize=512m -XX:+AggressiveOpts -Dfile.encoding=UTF-8"
-PATH=$PATH:$GRAILS_HOME/bin
+export GRAILS_HOME=/srv/grails
+export GRAILS_OPTS="-server -noverify -Xmx1G -Xms1G -XX:MaxPermSize=512m -XX:PermSize=512m -XX:+AggressiveOpts -Dfile.encoding=UTF-8"
+export PATH=$PATH:$GRAILS_HOME/bin
--- Shares/etc/profile.d/java.sh
+++ Shares/etc/profile.d/java.sh
@@ -1,2 +1,2 @@
-JAVA_HOME=/usr/lib/jvm/java-7-oracle
-PATH=$PATH:$JAVA_HOME/bin
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+export PATH=$PATH:$JAVA_HOME/bin
